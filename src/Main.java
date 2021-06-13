6f6f88e3763dab6457d3dda80c64bc7a33e23f15
Your name <Magorzata abendowicz>
2021-06-13 16:07:41 +0200

    ify

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
       Car Opel = new Car(1, 3.2, 3);
       Opel.calculateMaximumSpeed();
 
-
+    Car Seat = new Car("white","petrol","cos", 2004);
+    Seat.checkIffrom2000();
     }
 }

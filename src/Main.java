fb0d2c1b5ff127bbed89d257e8d01b020c70d854
Your name <Magorzata abendowicz>
2021-06-13 11:33:09 +0200

    drugi carobject

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String args[]){
         System.out.println("Hello World");
         System.out.println("Cześć Alicja");
+        System.out.print("Cześć Alicja");
+        System.out.print("Cześć Alicja");
+
+
     }
 }

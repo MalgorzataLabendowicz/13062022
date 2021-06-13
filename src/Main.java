52a6ab1762ef86f1c081ac15596c5c88c8f3aa6e
Your name <Magorzata abendowicz>
2021-06-13 15:27:30 +0200

    testing string in java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args){
-       String text = " olga ";
-       String text2 = new String("drugitekst");
-       String text3 = "olga3";
-       text.length();
-       System.out.println(text.length());
-        System.out.println(text.indexOf('g'));
-        System.out.println(text.trim());
-        System.out.println(text.substring(0,2));
-        text.toCharArray()
+      int a;
+      int b;
+      a = 14;
+      b = 10;
+      System.out.println(a+b);
+      System.out.println(a-b);
+      System.out.println(a*b);
+      System.out.println((double) a/b);
+      System.out.println(a%b);
     }
 }
